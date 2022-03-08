7f714b4156e2e6c3e3efbd8fe098ff2013103db5
rnwong <93447650+rnwong@users.noreply.github.com>
2022-03-07 18:39:11 -0800

    Update plotWave.m
    plot a waveform

--- plotWave.m
+++ plotWave.m
@@ -1,8 +1,8 @@
-function plotWave(waveform, fs, duration)
+function x = plotWave(waveform, fs, duration)
 % plotWave: plots a wave on a xy graph
 % Author: Rebecca Wong
 
-% when using, make sure your plot duration matches your waveform duration
+% when using, make sure your plot duration and fs match those of your waveform
 
     x = linspace(0, duration, fs*duration);
     plot(x, waveform);
