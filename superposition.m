8b223813b56d6a60b91cfe600a9c6c638d678d45
truongann <43941972+truongann@users.noreply.github.com>
2022-03-22 10:36:36 -0700

    Update superposition.m

--- superposition.m
+++ superposition.m
@@ -2,4 +2,4 @@ function superposition(selectedWave, sldDuration, sldAdjustment, sldVolume)
 
 baseSine = sine(sldAdjustment, sldDuration);
 
-sum = baseSine + selectedWave;
+superposition = baseSine + selectedWave;
